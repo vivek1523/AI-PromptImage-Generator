4d9b1caa0c94cabe2c09ef6956176a2e5678d5c8
vivek <vivekram4560@gmail.com>
2024-09-30 11:43:50 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -12,12 +12,13 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 
-// const corsOrigin = {
-//     origin: ["https://ai-promptimage-generator-client.onrender.com/"],
-// }
+const corsOrigin = {
+    origin: ["https://ai-promptimage-generator-client.onrender.com/"],
+    credentials:"true"
+}
 
 app.use(express.json());
-app.use(cors()); 
+app.use(cors(corsOrigin)); 
 
 mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log("MongoDB Connected Successfully");
