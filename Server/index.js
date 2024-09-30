0aefc95cab86f41676450e97dcb734ad000e3e8c
vivek <vivekram4560@gmail.com>
2024-09-30 11:32:07 +0530

    Update index.js

--- Server/index.js
+++ Server/index.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 4000;
 
 
 const corsOrigin = {
-    origin: ["http://localhost:5173"],
+    origin: ["https://ai-promptimage-generator-client.onrender.com/"],
 }
 
 app.use(express.json());
